897603b5f05e6849080d72b8604807f75ed9fc0b
KweonMo Koo <mikekoo2010@gmail.com>
2019-04-05 15:23:34 -0700

    Configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, "./client"), // where we want to transpile
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './client/dist')
+  },
+  mode: "development", // it tells that it will only transpile "production" changes
+  module: {
+    rules: [
+      {
+        loader: "babel-loader",
+        test: /\.js[x]?/, // forward slash blahblah and forward slash tells us regex
+        exclude: /node_modules/,
+        options: {
+          presets: ["@babel/preset-env", "@babel/preset-react"]
+        }
+      }
+    ]
+  },
+  resolve: { // for importing and exporting, if extensions are not attached it checks for those extensions
+    extensions: ['.js', 'jsx']
+  }
+};
