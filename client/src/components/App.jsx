69d4080c9613133e2f1a92cec03d9f819ab6bcdd
KweonMo Koo <mikekoo2010@gmail.com>
2019-04-06 14:03:36 -0700

    Show user notes and show form to create note upon write tab clicking

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,16 +1,56 @@
 import React from 'react';
+import NotesList from './NotesList.jsx';
+import Write from './Write.jsx'
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      notes: [{title: "Hello World", content: "display example"}, {title: "Need vacation", content: "To San Diego!"}],
+      myNotesTabVisibility: true,
+      writeTabVisibility: false
     }
+    this.handleMyNotesTab = this.handleMyNotesTab.bind(this);
+    this.handleWriteTab = this.handleWriteTab.bind(this);
+  }
+
+  handleMyNotesTab(e) {
+    this.setState({
+      myNotesTabVisibility: true,
+      writeTabVisibility: false
+    })
+  }
+
+  handleWriteTab(e) {
+    this.setState({
+      writeTabVisibility: true,
+      myNotesTabVisibility: false
+    })
   }
 
   render() {
+    let myNotes, writeForm;
+    this.state.myNotesTabVisibility ? myNotes = <NotesList notes={this.state.notes}/> : null;
+    this.state.writeTabVisibility ? writeForm = <Write /> : null;
+
     return (
-      <div>Hello</div>
+      <div className="notekeeper">
+        <div className="logo">Note Keeper</div>
+        <nav className="nav-bar">
+          <button 
+          className="mynotes-tab"
+          onClick={this.handleMyNotesTab}
+          >My Notes</button>
+          <button 
+          className="write-tab"
+          onClick={this.handleWriteTab}
+          >Write</button>
+        </nav>
+        <div className="form">
+          {myNotes}
+          {writeForm}
+        </div>
+      </div>
     )
   }
 }
