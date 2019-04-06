db5e8d6846023698e2c32ec97ed0b2534c89b349
KweonMo Koo <mikekoo2010@gmail.com>
2019-04-05 18:02:12 -0700

    Create CRUD operations and handle requests

---
+++ server/controllers.js
@@ -0,0 +1,50 @@
+const db = require('../database/dbHelper.js');
+
+module.exports = {
+  // retrieve all notes from database and send them to client
+  getNotes: (req, res) => {
+    db.fetch((err, notes) => {
+      if (err) {
+        console.error(err);
+        res.status(404).send(err);
+      } else {
+        res.status(200).json(notes);
+      }
+    })
+  },
+  // delete the note with given id
+  deleteNote: (req, res) => {
+    let { id } = req.body;
+    db.delete(id, (err) => {
+      if (err) {
+        console.error(err);
+        res.status(404).send(err);
+      } else {
+        res.status(202).end();
+      }
+    })
+  },
+  // create note in database to have it persist
+  postNote: (req, res) => {
+    let { content } = req.body;
+    db.post(content, (err) => {
+      if (err) {
+        console.error(err);
+        res.status(404).send(err);
+      } else {
+        res.status(201).end();
+      }
+    })
+  },
+  // edit the content of the note
+  updateNote: (req, res) => {
+    db.update(req.body, (err) => {
+      if (err) {
+        console.error(err);
+        res.status(404).send(err);
+      } else {
+        res.status(202).end();
+      }
+    })
+  }
+}
