121d1d097e8732eaac471888b4f631f4e4dfce85
KweonMo Koo <mikekoo2010@gmail.com>
2019-04-06 20:00:29 -0700

    Display time in Pacific Standard Timezone

--- client/src/components/NoteEntry.jsx
+++ client/src/components/NoteEntry.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import NoteStyle from './styles/NoteStyle.css'
 
 const NoteEntry = (props) => {
+  let tzd = new Date().getTimezoneOffset / 60;
+  let date = new Date(props.date);
+  let LADate = new Date(date.toUTCString());
+  
   return (
     <div className={NoteStyle.noteEntryWrapper}>
       <div className={NoteStyle.noteEntry}>
         <div className={NoteStyle.noteTitle}>{props.title}</div>
-        <div className={NoteStyle.noteDate}>{props.date}</div>
+        <div className={NoteStyle.noteDate}>{LADate.toString()}</div>
         <div className={NoteStyle.noteContent}>{props.content}</div>
       </div>
       <div className={NoteStyle.noteEntryControl} >
