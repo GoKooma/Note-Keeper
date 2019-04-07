9914d03d4f382b7b825be6166f5e32d730417094
KweonMo Koo <mikekoo2010@gmail.com>
2019-04-06 18:05:30 -0700

    Pass in index as a key, edit and delete functionalities

--- client/src/components/NotesList.jsx
+++ client/src/components/NotesList.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import NoteEntry from './NoteEntry.jsx'
+import BodyStyle from './styles/BodyStyle.css'
 
 const NotesList = (props) => {
   return (
-    <div className="notes-list">
-      {props.notes.map((note) => {
+    <div className={BodyStyle.notesList}>
+      {props.notes.map((note, index) => {
         return (
           <NoteEntry 
             title={note.title} 
             content={note.content} 
             date={note.date} 
+            id={index}
+            handleDelete={props.handleDelete}
+            handleUpdate={props.handleUpdate}
           />
         )
       })}
