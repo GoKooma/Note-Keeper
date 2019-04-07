55b82701600cf7ba4df1f9c635baaad3aac7d6ee
KweonMo Koo <mikekoo2010@gmail.com>
2019-04-06 18:03:58 -0700

    Add delete functionality and import style sheet

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,18 +2,21 @@ import React from 'react';
 import NotesList from './NotesList.jsx';
 import Write from './Write.jsx'
 import axios from 'axios';
+import BodyStyle from './styles/BodyStyle.css'
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      notes: [{title: "Hello World", content: "display example"}, {title: "Need vacation", content: "To San Diego!"}],
+      notes: [{title: "Welcome to Note Keeper!", content: "Loading..."}],
       myNotesTabVisibility: true,
       writeTabVisibility: false
     }
     this.handleMyNotesTab = this.handleMyNotesTab.bind(this);
     this.handleWriteTab = this.handleWriteTab.bind(this);
     this.fetchNotes = this.fetchNotes.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
+    this.handleUpdate = this.handleUpdate.bind(this);
   }
 
   componentDidMount() {
@@ -45,25 +48,46 @@ class App extends React.Component {
     })
   }
 
+  handleDelete(e, id) {
+    let noteID = this.state.notes[id]._id
+    e.preventDefault();
+    console.log(noteID)
+    axios
+      .delete(`/api/note/${noteID}`, { params: { id: noteID }})
+      .then(() => {
+        this.fetchNotes();
+      })
+      .catch(err => console.error(err));
+  }
+
+  handleUpdate(e, id) {
+    e.preventDefault();
+    console.log("EDIT THE NOTE")
+  }
+
   render() {
     let myNotes, writeForm;
-    this.state.myNotesTabVisibility ? myNotes = <NotesList notes={this.state.notes} /> : null;
+    this.state.myNotesTabVisibility ? myNotes = <NotesList 
+      notes={this.state.notes} 
+      handleDelete={this.handleDelete}
+      handleUpdate={this.handleUpdate}
+    /> : null;
     this.state.writeTabVisibility ? writeForm = <Write fetchNotes={this.fetchNotes} /> : null;
 
     return (
-      <div className="notekeeper">
-        <div className="logo">Note Keeper</div>
-        <nav className="nav-bar">
+      <div className={BodyStyle.notekeeper}>
+        <div className={BodyStyle.logo}>Note Keeper</div>
+        <nav className={BodyStyle.navBar}>
           <button 
-          className="mynotes-tab"
+          className={BodyStyle.tab}
           onClick={this.handleMyNotesTab}
           >My Notes</button>
           <button 
-          className="write-tab"
+          className={BodyStyle.tab}
           onClick={this.handleWriteTab}
           >Write</button>
         </nav>
-        <div className="form">
+        <div className={BodyStyle.contentBody}>
           {myNotes}
           {writeForm}
         </div>
