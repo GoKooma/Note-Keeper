3f02848615642f27da352f6546502408c08c0403
KweonMo Koo <mikekoo2010@gmail.com>
2019-04-06 18:04:50 -0700

    Add delete functionality and import style sheet

--- client/src/components/NoteEntry.jsx
+++ client/src/components/NoteEntry.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import NoteStyle from './styles/NoteStyle.css'
 
 const NoteEntry = (props) => {
   return (
-    <div className="note-entry">
-      <div className="title">{props.title}</div>
-      <div className="note-date">{props.date}</div>
-      <div className="content">{props.content}</div>
+    <div className={NoteStyle.noteEntryWrapper}>
+      <div className={NoteStyle.noteEntry}>
+        <div className={NoteStyle.noteTitle}>{props.title}</div>
+        <div className={NoteStyle.noteDate}>{props.date}</div>
+        <div className={NoteStyle.noteContent}>{props.content}</div>
+      </div>
+      <div className={NoteStyle.noteEntryControl} >
+        <button 
+          className={NoteStyle.button}
+          onClick={(e) => props.handleUpdate(e, props.id)}
+        >Edit</button>
+        <button 
+          className={NoteStyle.button}
+          onClick={(e) => props.handleDelete(e, props.id)}
+        >Delete</button>
+      </div>
     </div>
   )
 }
