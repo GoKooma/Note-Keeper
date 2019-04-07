5858a7eb3332a2c25506120f10aac9063a7423f9
KweonMo Koo <mikekoo2010@gmail.com>
2019-04-07 00:00:38 -0700

    Change variable name since JavaScript adjusts date to user's timezone

--- client/src/components/NoteEntry.jsx
+++ client/src/components/NoteEntry.jsx
@@ -2,15 +2,14 @@ import React from 'react';
 import NoteStyle from './styles/NoteStyle.css'
 
 const NoteEntry = (props) => {
-  let tzd = new Date().getTimezoneOffset / 60;
   let date = new Date(props.date);
-  let LADate = new Date(date.toUTCString());
+  let dateInUserTZ = new Date(date.toUTCString());
   
   return (
     <div className={NoteStyle.noteEntryWrapper}>
       <div className={NoteStyle.noteEntry}>
         <div className={NoteStyle.noteTitle}>{props.title}</div>
-        <div className={NoteStyle.noteDate}>{LADate.toString()}</div>
+        <div className={NoteStyle.noteDate}>{dateInUserTZ.toString()}</div>
         <div className={NoteStyle.noteContent}>{props.content}</div>
       </div>
       <div className={NoteStyle.noteEntryControl} >
