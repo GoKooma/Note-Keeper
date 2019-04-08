7f509e1b57cbdd2d54e4fa90277469461d24c3be
KweonMo Koo <mikekoo2010@gmail.com>
2019-04-08 02:48:12 -0700

    Use Write component to edit a user desired note

--- client/src/components/NotesList.jsx
+++ client/src/components/NotesList.jsx
@@ -1,21 +1,36 @@
 import React from 'react';
 import NoteEntry from './NoteEntry.jsx'
 import BodyStyle from './styles/BodyStyle.css'
+import Write from './Write.jsx';
 
 const NotesList = (props) => {
   return (
     <div className={BodyStyle.notesList}>
       {props.notes.map((note, index) => {
-        return (
-          <NoteEntry 
-            title={note.title} 
-            content={note.content} 
-            date={note.date} 
-            id={index}
-            handleDelete={props.handleDelete}
-            handleUpdate={props.handleUpdate}
-          />
-        )
+        if (props.noteToEdit !== index) {
+          return (
+            <NoteEntry 
+              title={note.title} 
+              content={note.content} 
+              date={note.date} 
+              id={index}
+              handleDelete={props.handleDelete}
+              notifyUpdate={props.notifyUpdate}
+            />
+          )
+        } else {
+          return (
+            <Write
+              fetchNotes={props.fetchNotes}
+              title={note.title}
+              content={note.content}
+              noteToEdit={props.noteToEdit}
+              editing={props.editing}
+              handleUpdate={props.handleUpdate}
+              id={index}
+            />
+          )
+        }
       })}
     </div>
   )
