74ecefd3cd3718c791ec8a9169462a179357cc75
KweonMo Koo <mikekoo2010@gmail.com>
2019-04-08 02:47:22 -0700

    Update method name according to App.jsx

--- client/src/components/NoteEntry.jsx
+++ client/src/components/NoteEntry.jsx
@@ -4,18 +4,20 @@ import NoteStyle from './styles/NoteStyle.css'
 const NoteEntry = (props) => {
   let date = new Date(props.date);
   let dateInUserTZ = new Date(date.toUTCString());
-  
+  // format date in a readable way
+  let datePosted = dateInUserTZ.toString().substring(0,21) + dateInUserTZ.toString().substring(33);
+
   return (
     <div className={NoteStyle.noteEntryWrapper}>
       <div className={NoteStyle.noteEntry}>
         <div className={NoteStyle.noteTitle}>{props.title}</div>
-        <div className={NoteStyle.noteDate}>{dateInUserTZ.toString()}</div>
+        <div className={NoteStyle.noteDate}>posted: {datePosted}</div>
         <div className={NoteStyle.noteContent}>{props.content}</div>
       </div>
       <div className={NoteStyle.noteEntryControl} >
         <button 
           className={NoteStyle.button}
-          onClick={(e) => props.handleUpdate(e, props.id)}
+          onClick={(e) => props.notifyUpdate(e, props.id)}
         >Edit</button>
         <button 
           className={NoteStyle.button}
